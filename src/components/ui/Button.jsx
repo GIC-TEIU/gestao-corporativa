066726579b949f412ba98f5bdba5e755eeeb85a6
stefani-teiu <desenvolvimento.web@teiu.com.br>
2025-09-23 14:57:08 -0300

    refactor(ui): Ajusta estilo e estrutura do componente Button

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -4,7 +4,8 @@ const Button = ({ children, ...props }) => {
   return (
     <button
       className="px-10 py-2 rounded-2xl border-2 border-brand-cyan font-medium
-                 text-white bg-brand-cyan hover:bg-brand-cyan hover:bg-opacity-90"
+                 text-white bg-brand-cyan hover:bg-brand-cyan hover:bg-opacity-90
+                 flex items-center justify-center gap-2"
       {...props} // type="submit"
     >
       {children}
